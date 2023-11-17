de56aed8c60df032532f6e1530e8cb3ab4eb2cef
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-11-17 13:42:46 -0800

    p

---
+++ wordcount.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+int countword(char *str)
+{
+	int i = 0, w = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] != ' ')
+		{	
+			w++;
+			while (str[i] != ' ' && str[i] != '\0')
+				i++;
+		}
+		else
+			i++;
+	}
+	return (w);
+}
