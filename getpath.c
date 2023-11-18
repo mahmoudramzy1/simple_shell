8e8c68c11acc7277aef1cae812dc137b0cc76e67
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-11-18 15:50:09 -0800

    p

---
+++ getpath.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+/**
+ * getpath - checks the path of the file
+ * @str: user's command
+ * Return: the full path of excutable file
+ */
+
+char *getpath(char *str)
+{
+	char *result;
+	char *tokenpath;
+	char *cpypath;
+	char *p = getenv("PATH");
+	
+	if(access(str, X_OK) == 0)
+		return(str);
+	if (p != 0)
+	{
+		cpypath = strdup(p);
+		tokenpath = strtok(cpypath, ":");
+		while(tokenpath)
+		{
+			result = malloc(strlen(tokenpath) + strlen(str) + 2);
+			strcpy(result, tokenpath);
+			strcat(result, "/");
+			strcat(result, str);
+			strcat(result, "\0");
+			if (access(result, X_OK) == 0)
+			{
+				free(cpypath);	
+				return (result);
+			}
+			free(result);
+			tokenpath = strtok(NULL, ":");
+		}
+	}
+	free(cpypath);
+       	return (NULL);
+}
--- simple_shell.c
+++ simple_shell.c
@@ -1,75 +1,25 @@
 #include "shell.h"
 
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <stdlib.h>
-#include <string.h>
 
-extern char **environ;
-
-char *getpath(char *str)
-{
-    char *result;
-    char *tokenpath;
-    char *cpypath;
-    char *p = getenv("PATH");
-    if(access(str, X_OK) == 0)
-        return(str);
-    if (p != 0)
-    {
-        cpypath = strdup(p);
-        tokenpath = strtok(cpypath, ":");
-        while(tokenpath)
-        {
-            result = malloc(strlen(tokenpath) + strlen(str) + 2);
-            strcpy(result, tokenpath);
-            strcat(result, "/");
-            strcat(result, str);
-            strcat(result, "\0");
-            if (access(result, X_OK) == 0)
-                return (result);
-	    free(result);
-            tokenpath = strtok(NULL, ":");
-
-
-        }
-	free(cpypath);
-
-    }
-
-    return (NULL);
-}
 
 
 int main(void)
 {
     pid_t child;
-    char *path;
-    int read = 0;
-    char *s = 0;
+    char *path, *s = 0;
+    int read = 0, i = 0, status;
     size_t len = 0;
     char **t;
-    int i = 0;
-    int status;
 	/*int exit_code = 0;*/
     while(1)
     {
         write(1, "$ ", 2);
         read = getline(&s, &len, stdin);
-	if (strcmp(s, "env") == 0)
-        {
-                while (environ[i])
-                        printf("%s\n", environ[i++]);
-        }
-	else
-	{
         if (read == -1)
-            break;
-        s[read - 1] = 0;
+		break;
+	s[read - 1] = 0;
 	t = argue(s);
-	/*if (strcmp(t[1], "exit") == 0)
+	/*if (strcmp(t[0], "exit") == 0)
 	{
 		if (t[1])
 			exit_code = atoi(t[1]);
@@ -77,6 +27,19 @@ int main(void)
 		free (s);
 		exit(exit_code);
 	}*/
+	if (strcmp(t[0], "env") == 0)
+        {
+                while (environ[i])
+		{
+		 	
+			printf("%s\n", environ[i]);
+			i++;
+		}
+		free(t);
+
+        }
+	else
+	{
         path = getpath(s);
 	if (path != NULL)
         {
@@ -85,20 +48,22 @@ int main(void)
         if (child == 0)
         {
                 execve(path, t, environ);
-
+		free(path);
                 exit(127);
 	}
 	else if (child < 0)
-		perror("fork error");
+	{	perror("fork error");
+		free(path);}
 	else
+	{
         	waitpid(child, &status, 0);
-        free(path);
+		free(path);}
 	}
 	else
 		printf("command not found\n");
-    	}
 	free(t);
+	}
+	free(s);
     }
-    free(s);
     return (0);
 }
